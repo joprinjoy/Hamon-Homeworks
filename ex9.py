eba5b2f4e1a9d5a40a8710d36202913c0cc3f3c3
joprin <joprinjoy3@gmail.com>
2024-05-22 23:59:15 +0530

    Printing Printing Printing -strings printed in next line -bigger string that may have two or more lines printed using '''

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#\n used to print the string in next line
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:",months)
+
+#thre chords used to insert as much lines as wanted
+
+print('''There is something going on here
+      we can print as much as wanted
+      multiple lines as long as wanted ''')
