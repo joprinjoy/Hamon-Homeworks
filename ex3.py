97d5f759b98252a8c9808adb721c88b6a6f00492
joprin <joprinjoy3@gmail.com>
2024-05-22 13:28:25 +0530

    Numbers and math -comments added

--- ex3.py
+++ ex3.py
@@ -1,10 +1,10 @@
-print("I will count my chickens:")
-print("hens",25 + 30 / 6)
+print("I will count my chickens:") #printing a string
+print("hens",25 + 30 / 6)   #printing a string along with the result of the arithmetic operation
 print("Roosters", 100 - 25 * 3 % 4)
 print("Now i will count eggs:")
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-print("Is it true that 3 + 2 < 5 - 7?")
-print(3 + 2 < 5 - 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #printing the result of arithmetic operation
+print("Is it true that 3 + 2 < 5 - 7?") #printing a string with the result of a logical operation
+print(3 + 2 < 5 - 7)    #printing result of a logical operation
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 print("Oh thats why its false")
