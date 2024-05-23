f576662f608a92e0f4983ba02c80c47805968ffd
joprin <joprinjoy3@gmail.com>
2024-05-23 12:40:09 +0530

    Prompting People -prompting message while taking input

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you ? ")
+height = input("How much tall are you ? ")
+weight = input("How much fo you weight? ")
+
+print(f"So, you are {age} old,{height} tall and {weight} heavy.")
