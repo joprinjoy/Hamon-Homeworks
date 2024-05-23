6062bfb3df4dfed729d07ef45616985d2b156d0c
joprin <joprinjoy3@gmail.com>
2024-05-23 12:12:26 +0530

    What was That -tabspaces -next line -printing backslashes -multi line printing with tabs

---
+++ ex10.py
@@ -0,0 +1,20 @@
+#/t used for printing tabspace before string
+tabby_cat = "\tI'm tabbed in."
+
+#\n used to print the rest next line
+persian_cat = "I'm split\non a line."
+
+#two backslashed used to print single backslash
+backslash_cat = "I'm \\ a \\ cat."
+
+#""" for multiple lines \t for adding spaces
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
